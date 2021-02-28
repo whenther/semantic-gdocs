02be6312cb045a978939cac5c18d266134e68925
Will Ockelmann-Wagner <will@willwow.dev>
2021-02-28 04:38:26 -0800

    add test to CI

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./App";
 
-test("renders learn react link", () => {
+import { App } from "./App";
+
+test("renders site", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText(/Google Docs HTML Cleaner/i);
+  expect(title).toBeInTheDocument();
 });
